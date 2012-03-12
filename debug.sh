b385963cf0f7b4dc678f37d5b78ea7637f1851e5
John Freeman <jfreeman@cse.tamu.edu>
2012-03-12 14:26:28 -0500

    change name to jfdoc

--- debug.sh
+++ debug.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 gmake
-# TODO: Fix error with call to rmdir
-rm -rf doc
-node --debug-brk jsdoc.js < jsdoc.js
+node --debug-brk jfdoc.js < $*
 
--- test.sh
+++ test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 gmake
-# TODO: Fix error with call to rmdir
-rm -rf doc
 gmake doc
 
